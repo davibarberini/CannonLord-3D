3a245192cedb74491481d514fe8869e14ed13054
davibarberini <davibarberini@gmail.com>
2019-12-27 16:47:51 -0300

    Enemy Life Bar, Added 2 more enemies

--- Assets/Scripts/enemyScript.cs
+++ Assets/Scripts/enemyScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class enemyScript : MonoBehaviour
 {
@@ -8,12 +9,14 @@ public class enemyScript : MonoBehaviour
     public int scoreAmount;
     public float velocity;
     public ParticleSystem deathParticle;
+    public Image lifeBar;
 
     Renderer rend;
     ParticleSystemRenderer particleRend;
     Transform t;
     GameObject[] wavePoints;
 
+    float maxVida;
     int count = 0;
     int voltas = 0;
     int maxVoltas = 2;
@@ -26,7 +29,10 @@ public class enemyScript : MonoBehaviour
         t = GetComponent<Transform>();
         rend = GetComponent<Renderer>();
         particleRend = deathParticle.GetComponent<ParticleSystemRenderer>();
+
         CheckColor();
+
+        maxVida = vida;
     }
 
     // Update is called once per frame
@@ -77,6 +83,9 @@ public class enemyScript : MonoBehaviour
                     }
                 }
             }
+
+            //Ajusta a barra de vida do objeto de acordo com a sua vida atual
+            SetLifeBar();
         }
     }
 
@@ -97,6 +106,12 @@ public class enemyScript : MonoBehaviour
         vida -= amount;
     }
 
+    //Muda a barra de vida de acordo com a vida atual
+    public void SetLifeBar()
+    {
+        lifeBar.fillAmount = 1 - ((maxVida - vida) / maxVida);
+    }
+
 
     //Muda a cor do inimigo de acordo com a sua vida atual
     public void CheckColor()
@@ -105,18 +120,32 @@ public class enemyScript : MonoBehaviour
         {
             rend.material.SetColor("_Color", Color.red);
             t.localScale = new Vector3(4, 4, 4);
+            velocity = 1;
         }
         else if(vida <= 20)
         {
             rend.material.SetColor("_Color", Color.green);
             t.localScale = new Vector3(5, 5, 5);
+            velocity = 0.9f;
         }
         else if (vida <= 30)
         {
             rend.material.SetColor("_Color", Color.yellow);
             t.localScale = new Vector3(6, 6, 6);
+            velocity = 0.8f;
         }
-
+        else if (vida > 30 && vida <= 60)
+        {
+            rend.material.SetColor("_Color", Color.blue);
+            t.localScale = new Vector3(7, 7, 7);
+            velocity = 0.7f;
+        }
+        else if (vida > 60 && vida <= 100)
+        {
+            rend.material.SetColor("_Color", Color.magenta);
+            t.localScale = new Vector3(8, 8, 8);
+            velocity = 0.6f;
+        }
     }
 
     public void CheckParticleColors(ParticleSystem particle)
@@ -132,8 +161,15 @@ public class enemyScript : MonoBehaviour
         }
         else if (vida <= 30)
         {
-            
             particleRend.material.SetColor("_Color", Color.yellow);
+        }
+        else if (vida > 30 && vida <= 60)
+        {
+            particleRend.material.SetColor("_Color", Color.blue);
+        }
+        else if (vida > 60 && vida <= 100)
+        {
+            particleRend.material.SetColor("_Color", Color.magenta);
         }
     }
 
